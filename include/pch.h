8d0019524d914f70a13467535b6c5e42d342509c
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-05-04 20:37:23 +0300

    Add precompiled header

---
+++ include/pch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef NDEBUG
+#define DEBUG 1
+#else
+#define DEBUG 0
+#endif
+
+#include <time.h>
+#include <math.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbit.h>
+#include <string.h>
+#include <syslog.h>
+#include <limits.h>
+#include <assert.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <arpa/inet.h>
+#include <uuid/uuid.h>
+#include <sys/socket.h>
