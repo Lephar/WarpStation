6fc50fc579ec7db1495f237497f94bd7589f23a9
Ali Emre Gülcü <aliemreglc@gmail.com>
2025-05-12 22:07:19 +0300

    Implement client list logic

--- include/client.h
+++ include/client.h
@@ -2,4 +2,5 @@
 
 #include "pch.h"
 
+void initClientList();
 void dispatchClient(int32_t fd, struct sockaddr_in addr);
--- src/client.c
+++ src/client.c
@@ -9,11 +9,56 @@ struct client {
     struct client *next;
 } typedef Client;
 
-Client *head = nullptr;
-Client *tail = nullptr;
+Client *head;
+Client *tail;
+
+void initClientList() {
+    head = malloc(sizeof(Client));
+    tail = malloc(sizeof(Client));
+
+    head->prev = nullptr;
+    head->conn = nullptr;
+    head->next = tail;
+
+    tail->prev = head;
+    tail->conn = nullptr;
+    tail->next = nullptr;
+}
+
+void addClient(Connection *conn) {
+    Client *client = malloc(sizeof(Client));
+
+    client->prev = tail->prev;
+    client->conn = conn;
+    client->next = tail;
+
+    tail->next = client;
+    tail->prev->next = client;
+}
+
+void removeClient(Connection *conn) {
+    Client *iter = head->next;
+
+    while(iter != tail) {
+        if(uuid_compare(iter->conn->uuid, conn->uuid) == 0) {
+            iter->prev->next = iter->next;
+            iter->next->prev = iter->prev;
+
+            destroyConn(iter->conn);
+
+            iter->prev = nullptr;
+            iter->conn = nullptr;
+            iter->next = nullptr;
+
+            free(iter);
+
+            return;
+        }
+    }
+}
 
 void *clientLoop(void *param) {
-    while (true) {
+    while(true) {
 
     }
 
